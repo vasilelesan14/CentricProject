e290a1f769e879ecfa4d13f35a3248200f6ec36a
Vasile Lesan <vasile.lesan14@gmail.com>
2025-05-27 19:02:30 +0300

    Adaugare pachete si drivere

---
+++ CentricUnitTestProject/CentricUnitTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace CentricUnitTestProject
+{
+    [TestClass]
+    public class CentricUnitTest
+    {
+
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Init()
+        {
+            try
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Setup failed: " + ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void SearchForParis_ShouldDisplayResults()
+        {
+            driver.Navigate().GoToUrl("https://www.trivago.com/en-US");
+
+            var searchBox = driver.FindElement(By.CssSelector("input[name='sQuery']"));
+            searchBox.SendKeys("Paris");
+
+            Thread.Sleep(2000); 
+
+            searchBox.SendKeys(Keys.Enter);
+
+            Thread.Sleep(4000); 
+
+            Assert.IsTrue(driver.PageSource.Contains("Paris"), "Rezultatele nu conțin 'Paris'");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ CentricUnitTestProject/packages/MSTest.3.9.1/PACKAGE.md
@@ -0,0 +1,14 @@
+# MSTest
+
+MSTest is Microsoft supported Test Framework.
+
+This package is a meta package that simplifies referencing all recommended MSTest packages.
+
+Supported platforms:
+
+- .NET 4.6.2+
+- .NET Core 3.1+
+- .NET 6.0+
+- .NET 6.0 Windows.18362+
+- UWP 10.0.16299
+- UWP 10.0.17763 with .NET 9+
