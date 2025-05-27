82ff53b62a2ec2a48282dd366c7191dd8d409807
Vasile Lesan <vasile.lesan14@gmail.com>
2025-05-28 00:57:04 +0300

    Flow 3: Modificare date cont

---
+++ CentricUnitTestProject/PageObjectModel/UpdateInfoPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace CentricUnitTestProject.PageObjectModel
+{
+    public class UpdateInfoPage
+    {
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+
+        public UpdateInfoPage(IWebDriver driver)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        public void GoToUpdateContactPage()
+        {
+            _driver.FindElement(By.LinkText("Update Contact Info")).Click();
+            _wait.Until(d => d.FindElement(By.XPath("//h1[text()='Update Profile']")));
+        }
+
+        public void UpdateContactInformation(string address, string city, string state, string zipCode, string phone)
+        {
+            _driver.FindElement(By.Id("customer.address.street")).Clear();
+            _driver.FindElement(By.Id("customer.address.street")).SendKeys(address);
+
+            _driver.FindElement(By.Id("customer.address.city")).Clear();
+            _driver.FindElement(By.Id("customer.address.city")).SendKeys(city);
+
+            _driver.FindElement(By.Id("customer.address.state")).Clear();
+            _driver.FindElement(By.Id("customer.address.state")).SendKeys(state);
+
+            _driver.FindElement(By.Id("customer.address.zipCode")).Clear();
+            _driver.FindElement(By.Id("customer.address.zipCode")).SendKeys(zipCode);
+
+            _driver.FindElement(By.Id("customer.phoneNumber")).Clear();
+            _driver.FindElement(By.Id("customer.phoneNumber")).SendKeys(phone);
+
+            _driver.FindElement(By.CssSelector("input[value='Update Profile']")).Click();
+
+            _wait.Until(d => d.FindElement(By.XPath("//*[contains(text(),'Your updated address and phone number have been added to the system.')]")));
+        }
+    }
+}
---
+++ CentricUnitTestProject/Tests/Workflow_3.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using CentricUnitTestProject.PageObjectModel;
+using CentricUnitTestProject.Date;
+using System.Resources;
+using System.Reflection;
+using System;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using CentricUnitTestProject.PageObjectModel;
+
+namespace CentricUnitTestProject.Tests
+{
+    [TestClass]
+    public class Workflow_3
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        private string baseUrl = "https://parabank.parasoft.com/parabank/index.htm";
+        private string username = "john";  // înlocuiește cu userul tău
+        private string password = "demo";  // înlocuiește cu parola ta
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("https://parabank.parasoft.com/parabank/index.htm");
+        }
+
+        [TestMethod]
+        public void UpdateContactInformationTest()
+        {
+            var loginPage = new LoginPage(driver);
+            var updateInfoPage = new UpdateInfoPage(driver);
+
+            loginPage.GoToLoginPage();
+            loginPage.Login("john", "demo");
+
+            updateInfoPage.GoToUpdateContactPage();
+            updateInfoPage.UpdateContactInformation(
+                "New Address St 100",
+                "New City",
+                "NewState",
+                "54321",
+                "0789123456"
+            );
+
+            Assert.IsTrue(driver.PageSource.Contains("Your updated address and phone number have been added to the system."),
+                "contact update message not found");
+        }
+
+
+        //[TestCleanup]
+        //public void Cleanup()
+        //{
+        //    driver.Quit();
+        //}
+    }
+}
